92d2d9902c1c1449316dbbb6049fe72229248fae
Jun Jie <96404859+CaKeNice@users.noreply.github.com>
2025-01-24 15:08:37 +0800

    Update vehicles.py

--- vehicles.py
+++ vehicles.py
@@ -3,8 +3,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.model_selection import train_test_split
 import joblib
 
 # # Define the model training function
